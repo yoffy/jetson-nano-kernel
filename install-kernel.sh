f5efc6897daac479048fc80a449f48a5381d5fe7
Kenta Yoshimura <yottui@yahoo.co.jp>
2020-01-05 13:05:19 +0900

    install-kernel.sh: fix wildcard expression.

--- install-kernel.sh
+++ install-kernel.sh
@@ -5,7 +5,7 @@ set -eu
 TEGRA_KERNEL_OUT=`pwd`/workdir/out
 
 sudo mv -n /boot/Image /boot/Image.orig
-sudo cp "${TEGRA_KERNEL_OUT}/arch/arm64/boot/Image" /boot/
-sudo cp "${TEGRA_KERNEL_OUT}/arch/arm64/boot/dts/*" /boot/
+sudo cp "${TEGRA_KERNEL_OUT}"/arch/arm64/boot/Image /boot/
+sudo cp "${TEGRA_KERNEL_OUT}"/arch/arm64/boot/dts/* /boot/
 #sudo mv -n /boot/extlinux/extlinux.conf /boot/extlinux/extlinux.conf.orig
 #sudo cp extlinux.conf /boot/extlinux/
